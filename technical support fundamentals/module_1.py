3d05745f6b91a668b660e6a17a833c9973d2eb70
Sharon Antuanet Ivet Barrial Marin <u202114900@upc.edu.pe>
2025-04-15 16:28:51 -0500

    Create module_1.py

---
+++ technical support fundamentals/module_1.py
@@ -0,0 +1,58 @@
+"""
+///FIRST ATTEMPT
+Question 1:
+Which of the following roles are typically considered part of IT?
+X Business analyst
+✅ Network engineer
+✅ Desktop support personnel
+✅ Hardware technician
+
+(All are part of IT roles.)
+
+Question 2:
+Fill in the blank: A(n) ____ is a device that stores and processes data by performing calculations.
+✅ computer
+
+Question 3:
+Which of these devices can be used to control electricity voltages in computers?
+✅ Transistors
+X Computer chips
+✅ Vacuum tubes
+❌ CD-Roms
+
+Question 4:
+Fill in the blank: There are ___ bits in a byte.
+✅ 8
+
+Question 5:
+Fill in the blank: ____ is the oldest character encoding standard and it uses 127 of the 256 possible bytes.
+✅ ASCII
+
+Question 6:
+Fill in the blank: In a binary system, an electrical signal indicates a value of ___.
+✅ 1
+
+Question 7:
+Convert decimal 123 to binary:
+✅ 01111011
+(128: 0, 64: 1, 32: 1, 16: 1, 8: 1, 4: 0, 2: 1, 1: 1)
+
+Question 8:
+Convert binary 00100001 to decimal:
+✅ 33
+(32 + 1 = 33)
+
+Question 9:
+Convert binary 10001001 to decimal:
+✅ 137
+(128 + 8 + 1 = 137)
+
+Question 10:
+Which of these things were designed to facilitate abstraction?
+✅ A car steering wheel
+X Logic gates
+✅ An error message from your computer’s browser
+❌ A textbook
+
+(A textbook explains abstraction, but isn’t a system abstraction itself.)
+"""
