dae28274f35eac0b717182d55db6e36e14cdf039
Sharon Antuanet Ivet Barrial Marin <u202114900@upc.edu.pe>
2025-04-15 17:07:23 -0500

    Create module_3.py
    second attempt = 100

---
+++ technical support fundamentals/module_3.py
@@ -0,0 +1,93 @@
+"""
+first attemp
+
+Question 1:
+You’re entering data in a spreadsheet and it slows down after opening a menu you haven't used in weeks. What’s the likely cause?
+✅ Your computer needs to load the page for that menu from virtual memory into RAM.
+
+Question 2:
+Why are virtual machines (VMs) popular in business settings?
+✅ VMs are easier to maintain and provision than physical machines.
+✅ Resources used by a VM can be reclaimed once the VM is no longer needed.
+✅ VMs allow a user to run multiple operating systems at once.
+❌ VMs don’t require physical resources – (They do; they just share them.)
+
+Question 3:
+Which of the following commands can be used to create a folder in Linux?
+✅ mkdir
+
+Question 4:
+What is Ubuntu’s main screen called?
+✅ Desktop
+
+Question 5:
+Data is written to the hard drive in the form of _____.
+✅ Data blocks
+
+Question 6:
+Which functions are carried out by an OS’s kernel?
+✅ Managing system resources by creating, scheduling, and managing processes.
+✅ Managing the input and output of external devices.
+si File storage and file management – (Handled more by file system components.)
+❌ GUI or CLI interaction – (Handled by shells, not the kernel itself.)
+
+Question 7:
+Which OS is packaged and distributed by various organizations?
+✅ Linux
+
+Question 8:
+Which of the following is true of Chrome OS?
+✅ It is designed for simplicity, security, and speed.
+
+Question 9:
+What process does the BIOS/UEFI run to ensure a computer is in proper working order before booting?
+✅ POST (Power-On Self-Test)
+
+Question 10:
+Command to create a file called Secret_document in Linux?
+✅ touch Secret_document
+
+/////////////////////////////////////////////////////////////////////////////////////////
+second attempt
+
+Question 1:
+You open an old menu and your spreadsheet slows down. What's the likely cause?
+✅ Your computer needs to load the page for that menu from virtual memory into RAM.
+
+Question 2:
+What is a virtual machine (VM)?
+✅ An application that uses physical resources like memory, CPU, and storage, but offers the added benefit of running multiple operating systems at once.
+
+Question 3:
+Command to create a folder called Folder1 in Linux?
+✅ mkdir Folder1
+
+Question 4:
+What is Windows’ main screen called?
+✅ Desktop
+
+Question 5:
+Which part of a file’s metadata tells a user what type of file it is?
+✅ Its file extension.
+
+Question 6:
+Which part of an OS interacts with a system’s hardware?
+✅ The kernel
+
+Question 7:
+Which of the following OSes is open source?
+✅ Linux
+
+Question 8:
+Which of the following is true of Chrome OS?
+✅ It receives automatic updates, which help to guard against malware and security hacks.
+
+Question 9:
+You want to boot an OS from a USB drive. Which menu do you use?
+✅ Boot Options menu
+
+Question 10:
+Command to create a file called START_HERE in Linux?
+✅ touch START_HERE
+
+""""
