4a00c27f3b76edf3ba7c18d3473a346795f18f07
Sharon Antuanet Ivet Barrial Marin <u202114900@upc.edu.pe>
2025-05-03 15:40:55 -0500

    feat: Create module1.py
    grade: 100%

---
+++ The Bits and Bytes of Computer Networking/module1.py
@@ -0,0 +1,51 @@
+"""
+✅ 1.
+True
+
+TCP ensures reliable data delivery through mechanisms like acknowledgments and retransmissions.
+
+✅ 2.
+Application
+
+It's the 5th layer in the TCP/IP Five-layer model.
+
+✅ 3.
+Network
+
+IP (Internet Protocol) operates at the Network layer.
+
+✅ 4.
+True
+
+Fiber optic cables transmit data using light pulses.
+
+✅ 5.
+Collision domain
+
+When multiple systems send data that interferes, it's a collision domain issue.
+
+✅ 6.
+Router
+
+A router forwards traffic between different networks.
+
+✅ 7.
+False
+
+Many devices can act as both clients and servers (e.g., a smartphone).
+
+✅ 8.
+Half Duplex
+
+Communication goes both ways, but only one direction at a time.
+
+✅ 9.
+Port lights
+
+Port (or link) lights on switches/routers provide useful troubleshooting data.
+
+✅ 10.
+Carrier Sense Multiple Access with Collision Detection (CSMA/CD)
+
+Used in Ethernet to manage access and avoid collisions.
+"""
