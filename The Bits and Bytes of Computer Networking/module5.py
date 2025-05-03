43e7e64de5c84b46e104faaf0a701404265ec449
Sharon Antuanet Ivet Barrial Marin <u202114900@upc.edu.pe>
2025-05-03 16:24:26 -0500

    feat: Create module5.py
    grade: 100

---
+++ The Bits and Bytes of Computer Networking/module5.py
@@ -0,0 +1,45 @@
+"""
+Question 1: A ___ rate is the measurement of how much data can be passed across a phone line in a second.
+
+Answer: baud
+
+Question 2: Which of the following attributes describe a broadband internet connection? (Choose all that apply)
+
+Answer:
+
+Always on
+
+Not dial-up
+
+Question 3: How many T1 lines make up one T3 line?
+
+Answer: 28
+
+Question 4: Which form of Digital Subscriber Line (DSL) features differing upload and download speeds?
+
+Answer: ADSL
+
+Question 5: What type of cable is used for cable broadband?
+
+Answer: Coaxial
+
+Question 6: What does ONT stand for?
+
+Answer: Optical Network Terminal
+
+Question 7: Which 802.11 specification was widely adopted first?
+
+Answer: 802.11b
+
+Question 8: WLAN access points act as a ___ between wireless and wired networks.
+
+Answer: Bridge
+
+Question 9: What security standard quickly replaced WEP in most places?
+
+Answer: WiFi Protected Access (WPA)
+
+Question 10: Cellular network towers (or cells) are configured in such a way so that they avoid what type of problem?
+
+Answer: Overlap
+"""
