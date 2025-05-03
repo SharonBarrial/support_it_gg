9c84066da92955fffba8f54bad3984d69e3e9793
Sharon Antuanet Ivet Barrial Marin <u202114900@upc.edu.pe>
2025-05-03 16:27:14 -0500

    feat: Create module6.py

---
+++ The Bits and Bytes of Computer Networking/module6.py
@@ -0,0 +1,45 @@
+"""
+Question 1: What is one type of built-in protocol mechanism?
+
+Answer: Error detection
+
+Question 2: What does ICMP stand for?
+
+Answer: Internet Control Message Protocol
+
+Question 3: Traceroute uses UDP packets by default on which of the following operating systems? (Choose all that apply)
+
+Answer:
+
+Mac OS
+
+Linux
+
+Question 4: When issuing the Netcat command, which flags will tell you if a connection to the porting question is possible or not?
+
+Answer: -z and -v flags
+
+Question 5: You need to find the Internet Protocol (IP) address for a website. Which command do you use on a Windows system?
+
+Answer: Nslookup
+
+Question 6: Most public Domain Name Servers (DNS) are available globally through which technology?
+
+Answer: Anycast
+
+Question 7: A company runs sensitive technologies locally, while entrusting less-sensitive technologies to a broader user base. Which cloud delivery model is being used?
+
+Answer: Hybrid
+
+Question 8: Internet Protocol (IP) v6 addresses are written out in which format?
+
+Answer: 8 groups of 16 bits
+
+Question 9: Compress the following IP address: 9930:cafa:0000:0052:04cc:7b7a:0037:27e6.
+
+Answer: 9930:cafa:0:52:4cc:7b7a:37:27e6
+
+Question 10: Which Internet Protocol (IP) v6 header field is used to determine the quality of service level for a datagram?
+
+Answer: Flow label
+"""
