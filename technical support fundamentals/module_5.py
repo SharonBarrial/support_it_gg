d7662701d3d4fd914b505e8fd3f8ae26e3b5ba67
Sharon Antuanet Ivet Barrial Marin <u202114900@upc.edu.pe>
2025-05-03 12:45:09 -0500

    feat: Create module_5.py
    grade: 100%

---
+++ technical support fundamentals/module_5.py
@@ -0,0 +1,46 @@
+"""1.
+Photo editors, social media apps, and word processors are all examples of:
+✅ Software
+
+2.
+Fill in the blank: _____ is software used to keep the core system running.
+✅ System software
+
+3.
+Why are compilers a key component of programming and modern computer languages?
+✅ They allow programs written in human-readable code to be compiled into machine instructions that can be run on many different types of CPU.
+
+4.
+Which of the following practices helps IT support specialists ensure that malware is not installed on company computers? Select all that apply.
+✅ Verifying that they trust the source of any software they download to company computers
+✅ Blocking individuals from installing software themselves and requiring administrator credentials for approval
+✅ Ensuring that all installed software is up to date
+
+5.
+Your computer is running version 0.4.00 of a software program. Which versions are more up-to-date?
+✅ 1.5.19
+✅ 0.4.05
+✅ 4.0.00
+
+(0.3.05 is older)
+
+6.
+Best way to uninstall Catstronaut on Windows:
+✅ Find the program in System settings -> Add or remove programs, then uninstall it
+
+7.
+Where to find the most up-to-date version of Mozilla Firefox?
+✅ Mozilla Firefox’s website
+
+8.
+Command to verify that GIMP is installed on Linux:
+✅ dpkg -s gimp
+
+9.
+Command to install Mozilla Firefox after updating repositories:
+✅ sudo apt-get install firefox-esr
+
+10.
+Command to uninstall GIMP:
+✅ sudo apt-get remove gimp
+"""
