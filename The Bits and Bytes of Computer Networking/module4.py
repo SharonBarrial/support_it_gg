335f88cb643482e2200173bdfeb32473c740d1f6
Sharon Antuanet Ivet Barrial Marin <u202114900@upc.edu.pe>
2025-05-03 16:15:17 -0500

    feat: Create module4.py

---
+++ The Bits and Bytes of Computer Networking/module4.py
@@ -0,0 +1,37 @@
+"""
+1. At the very lowest level, what data does a computer understand?
+✅ Binary numbers
+
+2. How many root name servers are there?
+✅ 13
+
+3. Which of these protocols is used by DNS?
+✅ User Datagram Protocol (UDP)
+
+Nota: DNS usa UDP en el puerto 53 por defecto, aunque a veces usa TCP (como para transferencias de zona).
+
+4. A DNS record that's used to redirect traffic from one domain name to another is known as a _______ record.
+✅ CNAME
+
+CNAME = Canonical Name
+
+5. A complete Fully Qualified Domain Name (FQDN) is limited to how many characters?
+✅ 255
+
+6. Along with an IP address, a subnet mask, and a name server, which of the following is the other thing required for a computer to operate on a network?
+✅ A gateway
+
+7. Network Address Translation (NAT) is typically implemented with which of these hardware devices?
+✅ Routers
+
+8. What technique is used with Network Address Translation (NAT) when a source port chosen by a client is the same port used by a router?
+✅ Preservation
+
+9. Fill in the blank: Most VPNs work by using the payload section of the _____ layer to carry an encrypted payload that contains an entire second set of packets.
+✅ transport
+
+Aunque lo más correcto sería “network”, no estaba en las opciones.
+
+10. A service that appears to external clients as a single server, but actually represents many servers behind it, is known as a _____.
+✅ reverse proxy
+"""
