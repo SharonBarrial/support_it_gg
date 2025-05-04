9a73e18995348c4bdd46b8addfc24979d13addd4
Sharon Antuanet Ivet Barrial Marin <u202114900@upc.edu.pe>
2025-05-03 20:56:57 -0500

    feat: Create module3.py

---
+++ System Administration and IT Infrastructure Services/module3.py
@@ -0,0 +1,27 @@
+"""
+Authentication ✅
+
+Authentication ✅
+
+✅ Something you know
+✅ Something you are
+✅ Something you have
+
+Phishing ✅
+
+Server authentication ✅
+
+Remote wipes ✅
+
+Ticket Granting Ticket ✅
+
+✅ Reduce overhead of password assistance
+✅ One set of credentials for the user
+✅ Reduce time spent on re-authenticating to services
+
+Compatibility with third party apps ✅
+
+✅ Track user authentication
+✅ Systems users authenticated to
+✅ Commands that were run
+"""
