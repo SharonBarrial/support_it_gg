c56927f0ba84fb998ad91704b6a1b6d73dd4acfe
Sharon Antuanet Ivet Barrial Marin <u202114900@upc.edu.pe>
2025-05-03 20:35:34 -0500

    feat: Create module1.py
    grade: 100%

---
+++ System Administration and IT Infrastructure Services/module1.py
@@ -0,0 +1,33 @@
+"""
+In the CIA triad, what does the letter ‘I’ stand for?
+✅ Integrity
+
+Which of the following is an example of a zero-day vulnerability?
+✅ A group of hackers finds a flaw in a new software product and takes advantage of it before the vendor or developer finds it.
+
+What kind of malware can block access to data and decrease the availability of security by holding the system hostage?
+✅ Ransomware
+
+What is it called when a hacker gets into a system through a secret entryway to gain remote access to the computer?
+✅ A backdoor
+
+An employee at a company plugs a router into the corporate network to make a simple wireless network. An attacker outside the building uses it to get access to the corporate network. What is the name of this type of attack?
+✅ A rogue AP (Access Point) attack
+
+Which of the following can occur during a ping of death (POD) attack? (Select all that apply)
+✅ Execution of malicious code
+✅ A buffer overflow
+
+What is it called when a hacker takes down multiple services very quickly with the help of botnets?
+✅ Distributed denial-of-service (DDoS)
+
+Which of the following measures can prevent injection attacks? (Select all that apply)
+✅ Data sanitization
+✅ Input validation
+
+What kind of attack keeps trying different combinations of characters and letters until it gets password access to a system?
+✅ A brute force attack
+
+An email message tells a user there is a problem with the user’s bank account. The email directs the user to a login page that steals the user’s information. What is this called?
+✅ Phishing
+"""
