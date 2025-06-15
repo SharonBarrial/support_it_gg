3035a739a420ec12e2a38744846c4aa21b1e0027
Sharon Antuanet Ivet Barrial Marin <u202114900@upc.edu.pe>
2025-06-15 00:47:35 -0500

    Create module4.py

---
+++ System Administration and IT Infrastructure Services/module4.py
@@ -0,0 +1,38 @@
+""""
+Question 6
+In Active Directory, which of the following can be functions of the Domain? (Choose all that apply)
+
+A server that holds a replica of the Active Directory database
+Correct
+
+A DNS server
+Correct
+
+A Kerberos authentication server
+Correct
+
+A container X 
+This should not be selected
+
+Question 3
+Instead of assigning access for each user account individually, ________ is a more efficient and easier-to-manage approach.
+
+active directory x
+centralized authentication X
+centralized management*
+LDAP
+
+*Which of the following is not an advantage of replication of data in terms of directory services?
+
+It decreases latency when accessing the directory service.
+It allows flexibility, allowing for easy creation of new object types as needs change. X
+It allows local management of user accounts.*
+It provides redundancy for data.
+
+*Which of these statements about Active Directory (AD) are true? (Choose all that apply)
+AD includes a tool called the Active Directory Authentication Center, or ADAC. x
+AD is incompatible with Linux, OS X, and other non-Windows hosts.
+AD can "speak" LDAP.*
+AD is used as a central repository of group policy objects, or GPOs.*
+
+""""
