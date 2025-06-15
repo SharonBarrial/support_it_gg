2f64ae5a8a85e819bf1dc53c5ca5a588b8678451
Sharon Antuanet Ivet Barrial Marin <u202114900@upc.edu.pe>
2025-06-15 02:35:41 -0500

    Create creatingacompanycultureforsecurity.py

---
+++ IT Security: Defense against the digital dark arts/creatingacompanycultureforsecurity.py
@@ -0,0 +1,4 @@
+""""
+Well, we could have an organization overview, with an authentication system. external website security as internal website security, that means including remote access solution and for security include a firewall and basic rules for maintenance recommendations, we can include wireless security and VLAN configuration, that includes laptop security configuration and application privacy policy recommendations
+
+""""
