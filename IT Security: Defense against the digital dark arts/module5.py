5094ef24d435a0cada580213f42f5c438c324fa9
Sharon Antuanet Ivet Barrial Marin <u202114900@upc.edu.pe>
2025-06-15 02:28:16 -0500

    Create module5.py

---
+++ IT Security: Defense against the digital dark arts/module5.py
@@ -0,0 +1,68 @@
+"""
+grade 100%
+
+Pregunta 1
+What is a class of vulnerabilities that are unknown before they are exploited?
+✅ Zero-day
+
+Pregunta 2
+Medidas para proteger un servidor de autenticación expuesto a internet:
+✅ Designate as a bastion host
+✅ Access Control Lists (ACLs)
+✅ Patch management
+✅ Secure firewall
+
+Pregunta 3
+Conexiones de muchos hosts Windows a un IP extranjero podrían indicar:
+✅ It can indicate a malware infection
+
+Pregunta 4
+Dos problemas principales con el software antivirus:
+✅ They depend on the antivirus vendor discovering new malware and writing new signatures for newly discovered threats.
+✅ They depend on antivirus signatures distributed by the antivirus software vendor.
+
+Pregunta 5
+¿Qué protege el cifrado de disco completo (FDE)?
+✅ Data theft
+✅ Data tampering
+
+❌ FDE no protege contra malware ni escuchas (eavesdropping) en tránsito.
+
+Pregunta 6
+¿Por qué instalar actualizaciones en tu computadora?
+✅ Updating adds new features
+✅ Updating improves performance and stability
+✅ Updating addresses security vulnerabilities
+
+Pregunta 7
+¿Cómo ayuda SCCM (o herramientas similares)?
+✅ Analyze installed software across multiple computers
+✅ Force update installation after a specified deadline
+✅ Confirm update installation
+
+❌ No está diseñado específicamente para detectar y prevenir malware como un antivirus.
+
+Pregunta 8
+¿Por qué es riesgoso permitir software de compartición de archivos?
+✅ The software could be infected with malware
+
+Pregunta 9
+Guardar de forma segura una clave de recuperación o respaldo se llama:
+✅ Key escrow
+
+Pregunta 10
+¿Por qué desactivar componentes innecesarios en sistemas y software?
+✅ Less complexity means less vulnerability.
+
+
+2.
+Question 2
+A core authentication server is exposed to the internet and is connected to sensitive services. What are some measures you can take to secure the server and prevent it from getting compromised by a hacker? Select all that apply.   
+
+
+Patch management x
+Designate as a bastion host*
+Access Control Lists (ACLs)* 
+Secure firewall* 
+
+"""
