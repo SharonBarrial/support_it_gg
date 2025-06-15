bbc91f01df2774f97665659d22d7fd97be729f6d
Sharon Antuanet Ivet Barrial Marin <u202114900@upc.edu.pe>
2025-06-15 01:48:21 -0500

    Create module2.py

---
+++ IT Security: Defense against the digital dark arts/module2.py
@@ -0,0 +1,24 @@
+"""
+6.
+Question 6
+You got an email from a company saying that the password hash associated with your account was retrieved by hackers. Which of the following options are possible? Select all that apply.
+
+
+If you used a common password, the hackers might be able to use a rainbow table to retrieve your original password.*
+The hackers are unable to use the hashed password because they are unable to use the password hash to recover plaintext.
+If they have the correct encryption key, the hackers can use the password hash to recover your original password.
+The hackers might be able to figure out your password through a very computationally intensive brute force attack.*
+
+6.
+Question 6
+Why have some security experts recommended replacing SHA1 with SHA2 or SHA3? Select all that apply. 
+
+Attacking SHA1 hashes is easy and takes little processing power x
+SHA1 hash collisions have been used to forge digital certificates. x
+A full hash collision of SHA1 has been published.*
+Attacking SHA1 hashes is expensive, but the cost is decreasing so attacks on SHA1 are becoming more feasible.*
+
+
+
+
+""""
