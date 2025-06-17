7f1ed026d8d00456b1aeeddb6473356eacd4479a
Sharon Antuanet Ivet Barrial Marin <u202114900@upc.edu.pe>
2025-06-17 06:59:56 -0500

    Update module2.py

--- IT Security: Defense against the digital dark arts/module2.py
+++ IT Security: Defense against the digital dark arts/module2.py
@@ -1,12 +1,167 @@
 """
-6.
+Question 1
+Studying how often letters and pairs of letters occur in a language is referred to as _______.
+codebreaking
+espionage
+cryptography
+frequency analysis*
+
+Question 2
+How can you make a symmetric encryption key more difficult to break?
+Use an initialization vector with the key.*
+Increase the length of the key.*
+Use the key fewer than five times.
+Make the key smaller.
+
+Question 3
+Fill in the blank: To create a public key signature, use the ______ key.
+public
+decryption
+private*
+symmetric
+
+Question 4
+A company wants to create a communication scheme that is both secure and efficient, using the benefits of both symmetric and asymmetric encryption. How would they do this?
+The company could use a symmetric encryption key that they transmit to other parties via asymmetric encryption.*
+It isn’t possible to use symmetric and asymmetric encryption at the same time.
+The company can use a message authentication check (MAC) to keep their information secure. 
+The company could use an asymmetric encryption key that they transmit to other parties via symmetric encryption. 
+
+Question 5
+Which of the following options are true of an ideal hash function? Select all that apply.
+It maps any amount of data to an output of fixed size.*
+Different inputs never result in the same output.*
+The same input always results in the same output.*
+It is possible to recover the original plaintext from the hash output.
+
+Question 6
+Why have some security experts recommended replacing SHA1 with SHA2 or SHA3? Select all that apply. 
+A full hash collision of SHA1 has been published.***
+Attacking SHA1 hashes is easy and takes little processing power
+SHA1 hash collisions have been used to forge digital certificates.
+Attacking SHA1 hashes is expensive, but the cost is decreasing so attacks on SHA1 are becoming more feasible.****
+
+Question 7
+The HTTPS protocol keeps internet traffic secure by encapsulating HTTP traffic over an encrypted channel that uses ____.
+SSH
+SSL
+PGP protocols
+TLS****
+
+Question 8
+You attend a key signing party to establish a web of trust. Later, you sign a colleague’s public key and they sign yours in return. What is your colleague’s relationship with the web of trust? Select all that apply.
+Your colleague must verify their identity with the root certificate authority. 
+When your colleague signs the certificates of other individuals, those individuals become part of the web of trust.***
+Your colleague is part of the web of trust.****
+When your colleague signs the certificates of other individuals you must also verify their identity. 
+
+Question 9
+Using an asymmetric cryptosystem provides which of the following benefits? Check all that apply.
+Confidentiality*
+Authenticity*
+Hashing 
+Non-repudiation*
+
+Question 10
+A cryptographic hash function is primarily used to:
+Generate a unique digital fingerprint of data***
+Encrypt data for secure transmission
+Decrypt encrypted data
+Create strong passwords
+
+Question 2
+ROT13 and a Caesar cipher are examples of _______.
+substitution ciphers*
+digital signatures 
+asymmetric encryption 
+steganography
+
+Question 4
+In which of the following scenarios is symmetric encryption better than asymmetric encryption? Select all that apply. 
+All communications will take place over a trusted network, like a company’s internal servers.***
+You need a digital signature for message verification. 
+Communications may take place over untrusted networks.
+You need to encrypt and decrypt a large amount of data.*** 
+
+Question 6
+Which of the following strategies can make passwords and password hashes stronger and more difficult for hackers to use?
+Using an uncommon password.****
+Using randomly chosen and large password salts.****
+Use passwords that are known to be strong. 
+Running passwords through the hashing function multiple times.****
+
+Question 7
+Select all that apply: In the TLS protocol, a client validates a server’s digital certificate by checking that the certificate:
+is for the correct host name.****
+is trusted.****
+lists the shared secret.
+supports the correct TLS version.
+
+Question 8
+____ is a system that defines the creation, storage, and distribution of digital certificates. 
+CA
+TLS
+PKI***
+VPN
+
+Question 9
+You’re a consultant whose clients send you documents encrypted with your public key. You use your private key to decrypt these documents. What would happen if the client uses the wrong public key to encrypt the file before sending it to you?
+You would be able to see the contents of the file.
+You would not be able to decrypt the file, so you will not be able to open it. x
+You would not be able to decrypt the file, so you will only be able to see a preview of the file’s contents.
+You would not be able to decrypt the file, so when you open it you will see garbled, nonsensical output.*
+
+Question 10
+If two different files result in the same hash, it is referred to as a ______.
+Hash collision*
+Key collision
+Mistake
+Coincidence
+
+Question 2
+DES, RC4, and AES are examples of ______ encryption algorithms.
+asymmetric
+symmetric
+weak
+strong
+
+Question 7
+Select all that apply: In the TLS protocol, a client validates a server’s digital certificate by checking that the certificate:
+is for the correct host name.*
+lists the shared secret.
+is trusted.*
+supports the correct TLS version.
+
+Question 9
+Which of the following is a primary security benefit of using CBC mode for encryption?
+Improved message integrity through dependency between ciphertext blocks*
+Smaller ciphertext size compared to other modes
+Enhanced resistance to brute-force attacks
+Increased encryption speed compared to other modes
+
+Question 3
+Which of the following are necessary components for encryption and decryption operations when using an asymmetric encryption system? Select all that apply.
+Private key***
+Digest
+public key signature
+Public key****
+
+Question 4
+In which of the following scenarios is asymmetric encryption better than symmetric encryption? Select all that apply. 
+All communications will take place over a trusted network, like a company’s internal servers.
+You need a digital signature for message verification.*
+You need to encrypt and decrypt a large amount of data. 
+Communications may take place over untrusted networks.-*
+
+
+
+
+
 Question 6
 You got an email from a company saying that the password hash associated with your account was retrieved by hackers. Which of the following options are possible? Select all that apply.
-
-
 If you used a common password, the hackers might be able to use a rainbow table to retrieve your original password.*
 The hackers are unable to use the hashed password because they are unable to use the password hash to recover plaintext.
-If they have the correct encryption key, the hackers can use the password hash to recover your original password.
+If they have the correct encryption key, the hackers can use the password hash to recover your original password.*
 The hackers might be able to figure out your password through a very computationally intensive brute force attack.*
 
 6.
